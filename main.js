5c6aa054df6425faa9e59315d3c1c47098ade5a0
Matteo Chanté-Biyikli <130081405+Matteo-CB@users.noreply.github.com>
2025-06-13 14:22:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+onload=()=>{const o=setTimeout(()=>{document.body.classList.remove("not-loaded"),clearTimeout(o)},1e3)};
